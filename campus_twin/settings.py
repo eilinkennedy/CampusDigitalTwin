4351d697e9a171e6496daf7f7aa57b2d4760c6c2
eilin kennedy <eilinkennedy1705@gmail.com>
2026-07-13 10:41:10 +0530

    test

--- campus_twin/settings.py
+++ campus_twin/settings.py
@@ -13,11 +13,11 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 import os
 from pathlib import Path
 
-from dotenv import load_dotenv
-
-# Load environment variables from .env when running locally.
-load_dotenv()
-
+try:
+    from dotenv import load_dotenv
+    load_dotenv()
+except ImportError:
+    pass
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
