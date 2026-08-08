18a516d1a9eb5ee137e57fd1ee35efc966a7009a
eilin kennedy <eilinkennedy1705@gmail.com>
2026-08-08 16:58:48 +0530

    Allow Vercel host for Django

--- campus_twin/settings.py
+++ campus_twin/settings.py
@@ -38,7 +38,7 @@ SECRET_KEY = os.environ.get(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "1") == "1"
 
-ALLOWED_HOSTS = [ALLOWED_HOSTS = [
+ALLOWED_HOSTS =  [
     'localhost',
     '127.0.0.1',
     '.vercel.app',
